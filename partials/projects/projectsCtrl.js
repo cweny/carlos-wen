7f351c59bd7095ae6e99c93f3ef8d7cce8d98165
cweny <carlos.wen30@gmail.com>
2015-12-21 23:06:17 +0000

    added timeling

--- partials/projects/projectsCtrl.js
+++ partials/projects/projectsCtrl.js
@@ -4,19 +4,28 @@
     .controller('ProjectsCtrl', function ($scope) {
     var projects = [ {
         title: "Message REST Web Service",
-        url: "#/projects/message"
+        url: "#/projects/message",
+        time: "August 2015"
       }, {
         title: "Connect 4",
-        url: "#/projects/connect4"
+        url: "#/projects/connect4",
+        time: "December 2014"
       }, {
         title: "MozStumbler",
-        url: "#/projects/mozstumbler"
+        url: "#/projects/mozstumbler",
+        time: "October 2014"
       }, {
         title: "QR Code App",
-        url: "#/projects/qr"
+        url: "#/projects/qr",
+        time: "June 2014"
       }, {
         title: "Electromagnetic Tether Robot",
-        url: "#/projects/robot"
+        url: "#/projects/robot",
+        time: "February 2014"
+      }, {
+        title: "Others",
+        url: "#/projects/others",
+        time: "December 2013"
       }
     ];
     $scope.projects = projects;
