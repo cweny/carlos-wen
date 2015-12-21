cbc0f8ef1e7a35afb68249b161bc64177a33d6b8
cweny <carlos.wen30@gmail.com>
2015-12-21 18:15:03 +0000

    Changed project to use angular js and added partial html files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+(function () {
+  'use strict';
+  var app = angular.module('profile',[]);
+  app.controller('AppCtrl', function ($scope) {
+    $scope.test = "resume.html";
+    var tabs = [{
+        title: "Home",
+        url: "partials/home.html"
+      }, {
+        title: "Projects",
+        url: "partials/projects.html"
+      }, {
+        title: "Technical Experience",
+        url: "partials/experience.html"
+      }
+    ];
+    $scope.tabs = tabs;
+    $scope.selectedUrl = "partials/home.html";
+    $scope.onClick = function(tab) {
+      $scope.selectedUrl = tab.url;
+    }
+  });
+
+})();
