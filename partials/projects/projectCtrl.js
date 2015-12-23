6a0323a3177061f2a6a98cba61c562e6e3b70786
cweny <carlos.wen30@gmail.com>
2015-12-23 21:52:24 +0000

    updated routing params and added validation check

--- partials/projects/projectCtrl.js
+++ partials/projects/projectCtrl.js
@@ -4,9 +4,21 @@
     .controller('ProjectCtrl', function ($scope,$routeParams,$http,$sce) {
     $scope.init = function() {
       $http.get('partials/projects/projects.json').success(function(data) {
-        $scope.project = data[$routeParams.project_id];
-        for(var i=0; i<$scope.project.description.length; i++) {
-          $scope.project.description[i] =  $sce.trustAsHtml($scope.project.description[i]);
+        if($routeParams.project_id<1 || $routeParams.project_id>data.length) {
+          $scope.project = {
+            "title":"Project Not Found",
+            "time":"",
+            "description":[""],
+            "type":"",
+            "github":false,
+            "github_url":"",
+            "accomplishments":[]
+          };
+        } else {
+          $scope.project = data[$routeParams.project_id-1];
+          for(var i=0; i<$scope.project.description.length; i++) {
+            $scope.project.description[i] =  $sce.trustAsHtml($scope.project.description[i]);
+          }
         }
       });
     }
--- partials/projects/projectsCtrl.js
+++ partials/projects/projectsCtrl.js
@@ -6,7 +6,7 @@
       $http.get('partials/projects/projects.json').success(function(data) {
         $scope.projects = data;
         for(var i=0; i<$scope.projects.length; i++) {
-          $scope.projects[i].url = "#/projects/" + i;
+          $scope.projects[i].url = "#/projects/" + (i+1);
         }
       });
     }
