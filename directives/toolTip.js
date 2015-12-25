4ca1de31b8588f3ef8af5d8bc5a23ee36c6aa9ec
cweny <carlos.wen30@gmail.com>
2015-12-25 05:57:42 +0000

    Moved tool tip directive. Updated json file

---
+++ directives/toolTip.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+  angular.module('profile')
+  .directive('bsTooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
+  });
+})();
